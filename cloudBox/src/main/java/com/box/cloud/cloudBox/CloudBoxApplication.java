2118526262dbdda1270e079c38fb7ab685431181
huchengxie <854575570@qq.com>
2018-05-24 13:34:51 +0800

    第er次提交

---
+++ cloudBox/src/main/java/com/box/cloud/cloudBox/CloudBoxApplication.java
@@ -0,0 +1,12 @@
+package com.box.cloud.cloudBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBoxApplication.class, args);
+	}
+}
